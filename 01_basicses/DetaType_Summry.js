140d89996640ba6b89a2db9842720f5eda72911b
Hema <157248702+CodeLoverhema@users.noreply.github.com>
2024-08-07 18:48:16 +0000

    new

---
+++ 01_basicses/DetaType_Summry.js
@@ -0,0 +1,45 @@
+//# Primitive
+
+// 7 types : String, Number, Boolearn, Null, Undefine, Symbol,BigInt
+
+
+const score = 100
+
+const scoreValue = 100.55
+
+
+const userlogin = false
+
+
+const  outsaid = null
+
+let userEmail = Undefind;
+
+
+const id = Symbol('hema')
+const anotherid = Symbol('hema')
+
+
+
+console.log(id === anotherid)
+
+
+const bigNumber = 45454545454544454n
+
+
+
+
+//# Reference (Non Primitive)
+
+
+//Array, Objects, Functions
+
+
+const heros = ["salmankhan","amitabacchan","govinda"];
+
+
+  let myojb = {
+    name: "hema",
+    age:25,
+
+}
