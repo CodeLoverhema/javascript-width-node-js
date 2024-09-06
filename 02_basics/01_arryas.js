ab9c604ba558f160fcd65daddb3b8dbbde565742
Hema <157248702+CodeLoverhema@users.noreply.github.com>
2024-09-06 15:47:05 +0000

    arryas

--- 02_basics/01_arryas.js
+++ 02_basics/01_arryas.js
@@ -3,7 +3,26 @@
 const todayMyArr = [0,1,2,3,4,5]
 const todayNews = ["Aajtak","ABP News"]
 
-console.log(todayMyArr)
-console.log(todayNews)
+// console.log(todayMyArr)
+// console.log(todayNews)
 
-const todayMyArr2 =
+const todayMyArr2 = new Array (1,2,3,4,6)
+// console.log(todayMyArr2)
+
+const todayMyArr3 = new Array("hema","neetu","megha","maya")
+console.log(todayMyArr3[1])
+
+
+// Array Methods //
+
+// todayMyArr3.push(6)
+// todayMyArr3.push(9)
+// todayMyArr3.pop()
+
+// todayMyArr3.unshift(6)
+// todayMyArr3.shift(6)
+
+
+todayMyArr3.unshift(6)
+console.log()
+console.log(todayMyArr3);
